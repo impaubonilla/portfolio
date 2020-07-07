6a429da0c32436aad4ab4952f2c1607cb9267f77
Pau Bonilla <paulo111995@gmail.com>
2020-07-06 23:51:52 -0700

    added robots.txt

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,7 @@ export default function Projects() {
     return (
         <div>
             Projects
+            <a href="https://impaubonilla.github.io/tic-tac-toe/" target="_blank" rel="noopener noreferrer">text</a>
         </div>
     )
 }
