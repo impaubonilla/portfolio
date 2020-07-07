62ce4291d854dfdfd91f025fdb9dafd33ee745d9
Pau Bonilla <paulo111995@gmail.com>
2020-07-07 01:07:37 -0700

    Updates

---
+++ precache-manifest.91253a20cfb7dd84106db66fd384e670.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54b38ffa84e9491083a96bd50767c34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f15b391cba2c4c4494d",
+    "url": "/static/css/main.4e80b47c.chunk.css"
+  },
+  {
+    "revision": "eccc04ba53454566307f",
+    "url": "/static/js/2.7f6623ff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7f6623ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f15b391cba2c4c4494d",
+    "url": "/static/js/main.7baa90db.chunk.js"
+  },
+  {
+    "revision": "2df165a20f8c2d0709c4",
+    "url": "/static/js/runtime-main.f793350c.js"
+  },
+  {
+    "revision": "21e4d7b9c14a178884acd45040aaefe7",
+    "url": "/static/media/SF.21e4d7b9.jpeg"
+  },
+  {
+    "revision": "2b439e00418b7f2dbe6d5cc9f6e284a1",
+    "url": "/static/media/_white.2b439e00.png"
+  },
+  {
+    "revision": "363b826621461143285c974997072cd6",
+    "url": "/static/media/alchemist.363b8266.jpg"
+  },
+  {
+    "revision": "97b5e3ca7e976ae2227c59519fcd492c",
+    "url": "/static/media/baybayintagalog.97b5e3ca.mp4"
+  },
+  {
+    "revision": "9e4fe7bd6f2f358bd518ba0a1a9b747f",
+    "url": "/static/media/bestfriends.9e4fe7bd.jpg"
+  },
+  {
+    "revision": "8724bca519622ad6fc14e9b6259cfc7d",
+    "url": "/static/media/bobatime.8724bca5.jpeg"
+  },
+  {
+    "revision": "ab4aee89f25100261abd9d71cc843a2f",
+    "url": "/static/media/converse.ab4aee89.jpeg"
+  },
+  {
+    "revision": "b847e36d435192693c9045a5a4555c01",
+    "url": "/static/media/lakershat.b847e36d.jpeg"
+  },
+  {
+    "revision": "25f3ff6bbb6faa20cea33389c4301452",
+    "url": "/static/media/lasubway.25f3ff6b.jpg"
+  },
+  {
+    "revision": "134a25be0910783eac407345cf6ef1fc",
+    "url": "/static/media/ps4controller1.134a25be.JPG"
+  }
+]);
--- src/theme/theme.js
+++
@@ -1,7 +0,0 @@
-export default {
-    mediaQueries: {
-        bellow768: `only screen and (max-width: 768px)`,
-        bellow425: `only screen and (max-width: 425px)`,
-        bellow380: `only screen and (max-width: 380px)`
-    }
-}
