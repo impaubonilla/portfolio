2d2a699e53fcdcfeeeac5fff0157e863b90c928e
Pau Bonilla <paulo111995@gmail.com>
2020-07-07 01:00:45 -0700

    modified component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Projects() {
     return (
         <div>
-            Projects
+            Proyekto
             <a href="https://impaubonilla.github.io/tic-tac-toe/" target="_blank" rel="noopener noreferrer">text</a>
         </div>
     )
