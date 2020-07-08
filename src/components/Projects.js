ce9d71bb73163d733a24bdee5c3e1925cbe96927
Pau Bonilla <paulo111995@gmail.com>
2020-07-08 01:51:30 -0700

    added projects folder on images; modified Proj.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import ttt from '../images/projects/tic-tac-toe.png'
+import styled from 'styled-components'
+
+export const ProjectsContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #e8e8e8;
+
+  ul {
+      list-style: none;
+      li {
+          margin: 2rem;
+      }
+  }
+`
+
+export const ProjcetsImage = styled.img`
+  width: 100%;
+`
 
 export default function Projects() {
     return (
         <div>
             Proyekto
-            <a href="https://paubonilla.github.io/tic-tac-toe/" target="_blank" rel="noopener noreferrer">text</a>
+            <ProjectsContent>
+                <ProjcetsImage src={ttt} alt="tic tac toe" />
+                <div>
+                    <p>A The Office theme tic tac toe game. This game is built with React, HTML5, CSS3, SASS, Styled Components, and Javascript. </p>
+                </div>
+                <ul>
+                    <li><a href="https://paubonilla.github.io/tic-tac-toe/" target="_blank" rel="noopener noreferrer">Play Game</a></li>
+                    <li><a href="https://github.com/paubonilla/tic-tac-toe" target="_blank" rel="noopener noreferrer">View code</a></li>
+                </ul>
+            </ProjectsContent>
         </div>
     )
 }
