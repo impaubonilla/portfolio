08c110c10dbc8148a2752016f790af2675e03dc4
Pau Bonilla <paulo111995@gmail.com>
2020-07-19 16:40:25 -0700

    initial commit

--- src/projectfiles/ProjectFiles.js
+++ src/projectfiles/ProjectFiles.js
@@ -19,3 +19,21 @@ export const TicTacToe = () => {
     </ProjectWrapper>
   )
 }
+
+export const DMVTest = () => {
+  return (
+    <ProjectWrapper>
+      <ProjectContent>
+        <ProjcetImageStyle src={ttt} alt="tic tac toe" />
+        <div className="desc">
+          <h2>DMV Permit Practice Test</h2>
+          <p>A The Office theme tic tac toe game. This game is built with React, HTML5, CSS3, SASS, Styled Components, and Javascript. </p>
+          <ul className="buttons">
+            <li><ALink primary href="https://paubonilla.github.io/dmv-permit-test/" target="_blank" rel="noopener noreferrer">Take Test</ALink></li>
+            <li><ALink href="https://github.com/paubonilla/dmv-permit-test" target="_blank" rel="noopener noreferrer"><p>View code</p></ALink></li>
+          </ul>
+        </div>
+      </ProjectContent>
+    </ProjectWrapper>
+  )
+}
