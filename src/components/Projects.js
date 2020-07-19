930c6409213651210867b5080f0e4ec31c0b3ea6
Pau Bonilla <paulo111995@gmail.com>
2020-07-19 16:56:24 -0700

    modified components

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { TicTacToe } from '../projectfiles/ProjectFiles'
+import { DMVTest } from '../projectfiles/ProjectFiles'
 
 export default function Projects() {
     return (
         <div>
             <TicTacToe />
+            <DMVTest />
         </div>
     )
 }
--- src/projectfiles/ProjectFiles.js
+++ src/projectfiles/ProjectFiles.js
@@ -24,10 +24,10 @@ export const DMVTest = () => {
   return (
     <ProjectWrapper>
       <ProjectContent>
-        <ProjcetImageStyle src={ttt} alt="tic tac toe" />
+        <ProjcetImageStyle src={ttt} alt="dmv permit practice test" />
         <div className="desc">
           <h2>DMV Permit Practice Test</h2>
-          <p>A The Office theme tic tac toe game. This game is built with React, HTML5, CSS3, SASS, Styled Components, and Javascript. </p>
+          <p>This test is built with React, HTML5, CSS3, SASS, Styled Components, and Javascript. </p>
           <ul className="buttons">
             <li><ALink primary href="https://paubonilla.github.io/dmv-permit-test/" target="_blank" rel="noopener noreferrer">Take Test</ALink></li>
             <li><ALink href="https://github.com/paubonilla/dmv-permit-test" target="_blank" rel="noopener noreferrer"><p>View code</p></ALink></li>
