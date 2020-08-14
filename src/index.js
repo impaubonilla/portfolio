0c34f1a6d03f8b1d4baff19cdf26f21b2eb8487c
Pau Bonilla <paulo111995@gmail.com>
2020-08-14 15:26:19 -0700

    removed unuse codes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { ThemeProvider } from 'styled-components';
 import './index.scss';
 import App from './App';
 import Theme from './costomization/Theme/theme'
-// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <ThemeProvider theme={Theme}>
@@ -14,8 +13,3 @@ ReactDOM.render(
   </ThemeProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
