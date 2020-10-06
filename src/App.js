3a31e39e9fe0a1a9ea458ae92490fcdd6f4ab284
Pau Bonilla <paulo111995@gmail.com>
2020-10-05 23:15:03 -0700

    added preloader

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 // Components
 import Nav from './components/Nav/Nav'
 import Content from './components/Content/Content'
 // Styles 
 import { PageWrapper, PageInner } from './costomization/Styled'
 import './App.scss'
+import { Cube } from 'styled-loaders-react'
+
+import styled from 'styled-components';
+
+export const Inner = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => { setLoading(false); }, 3000)
+  }, [])
   return (
-    <PageWrapper>
-      <PageInner>
-        <Nav />
-        <Content />
-      </PageInner>
-    </PageWrapper>
+    <>
+      {loading ?
+        <Inner>
+          <Cube />
+        </Inner>
+        :
+        <PageWrapper>
+          <PageInner>
+            <Nav />
+            <Content />
+          </PageInner>
+        </PageWrapper>}
+    </>
   );
 }
 
